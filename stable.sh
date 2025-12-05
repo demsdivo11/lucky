c60dc7ad06d8ebe61292e1a766697526c9bd3ae6
demsdev <161101830+demsdivo11@users.noreply.github.com>
2025-12-05 18:02:30 +0700

    Enhance proxy installation script with colors
    Added color formatting to installation messages.

--- stable.sh
+++ stable.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+RED='\033[31m'
+GREEN='\033[32m'
+ENDCOLOR='\033[0m'
+
 clear
-echo "Installing Proxy..."
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
 sleep 1
 
 if [ -f "Lucky271" ]; then
-    echo "Deleting old proxy..."
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
     rm Lucky271
     sleep 1
-    echo "Getting new proxy..."
+    echo -e "${GREEN}Getting new proxy...${ENDCOLOR}"
 fi
 
 wget -q https://github.com/demsdivo11/lucky/raw/main/Lucky271
 
 sleep 1
-echo "Proxy Installed"
-echo "Execute proxy with this command: ./Lucky271"
+echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./Lucky271${ENDCOLOR}"
 chmod +x Lucky271
